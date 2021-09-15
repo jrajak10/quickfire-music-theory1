bfbcaf63c5a784283d97f6a2cd55f0f1dddb00d6
Johann Rajakarunanayake <johannrajakarunanayake@Johanns-MacBook-Pro.local>
2021-09-15 18:54:38 +0100

    simplified code

--- notes.js
+++ notes.js
@@ -1,44 +1,35 @@
 
 
+function randomNote(notesArray) {
+    let randomNum;
+    for (let i = 0; i < notesArray.length; i++) {
+        randomNum = Math.floor(Math.random(i) * notesArray.length)
+    }
+    return notesArray[randomNum]
+}
 
 
+function createImage(notesArray) {
+    let newImage = document.createElement("IMG");
+    newImage.setAttribute("src", randomNote(notesArray));
+    newImage.setAttribute("id", "note");
+    newImage.setAttribute("height", "250");
+    newImage.setAttribute("width", "250");
+    newImage.setAttribute("alt", "Note");
+    document.getElementById("image").appendChild(newImage);
+}
 
 function clickStartButton() {
     let notesArray = ['C.png', 'E.png', 'G.png'];
 
+    randomNote(notesArray);
 
-    function randomNote(notesArray){
-        let randomNum;
-        for(let i=0; i< notesArray.length; i++){
-            randomNum = Math.floor(Math.random(i) * notesArray.length)
-        }
-        return notesArray[randomNum]
+    if (document.getElementById("image").childNodes.length <= 8) {
+        createImage(notesArray);
     }
-    
-
-    
-   
-    
-    if (document.getElementById("image").childNodes.length <= 8){
-        let newImage = document.createElement("IMG");
-        newImage.setAttribute("src", randomNote(notesArray));
-        newImage.setAttribute("id", "note");
-        newImage.setAttribute("height", "250");
-        newImage.setAttribute("width", "250");
-        newImage.setAttribute("alt", "Note");
-        document.getElementById("image").appendChild(newImage);
-    }
-    else{
+    else {
         let note = document.getElementById("note")
         note.parentNode.removeChild(note);
-        let newImage = document.createElement("IMG");
-        newImage.setAttribute("src", randomNote(notesArray));
-        newImage.setAttribute("id", "note");
-        newImage.setAttribute("height", "250");
-        newImage.setAttribute("width", "250");
-        newImage.setAttribute("alt", "Note");
-        document.getElementById("image").appendChild(newImage);
+        createImage(notesArray);
     }
-   
-    
 }
